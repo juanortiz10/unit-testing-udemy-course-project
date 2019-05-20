81c75e5ba0bf23f9b67801213a13bf96e8f0ac50
juanortiz10 <juanjo.jjo94@gmail.com>
2019-05-20 10:29:26 -0500

    Creating and adding styles to login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { withStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
 
-import { styles} from "./styles";
+import { styles } from "./styles";
 
 class Login extends Component {
   render() {
@@ -9,7 +11,9 @@ class Login extends Component {
 
     return(
       <div className={classes.container}>
-        Iniciar sesion
+        <Card className={classes.card}>
+          <Typography variant="h1" className={classes.title}>Iniciar sesion</Typography>
+        </Card>
       </div>
     )
   }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,3 +1,23 @@
 export const styles = {
-  container: {}
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    overflow: "hidden",
+    width: "100%"
+  },
+  card: {
+    width: "400px",
+    minWidth: "300px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    marginTop: 12,
+    fontSize: 18,
+    marginBottom: 4
+  }
 }
