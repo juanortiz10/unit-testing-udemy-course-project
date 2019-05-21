98cd73be5c96d3d9766c73966a5e361643b88639
juanortiz10 <juanjo.jjo94@gmail.com>
2019-05-21 10:34:16 -0500

    Add github login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import { withStyles } from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
+import GithubLogin from "react-github-login";
 
 import { styles } from "./styles";
 
 class Login extends Component {
+  onSuccess = response => {
+
+  }
+
+  onFailure = response => {
+
+  }
+
   render() {
     const { classes } = this.props;
 
@@ -13,6 +22,7 @@ class Login extends Component {
       <div className={classes.container}>
         <Card className={classes.card}>
           <Typography variant="h1" className={classes.title}>Iniciar sesion</Typography>
+          <GithubLogin className={classes.button} clientId={null} onSucess={this.onSucess} onFailure={this.onFailure} buttonText="Github account"/>
         </Card>
       </div>
     )
