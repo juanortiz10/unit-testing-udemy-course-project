5845ff383253eb96051e6a3acae49b6a843b9045
juanortiz10 <juanjo.jjo94@gmail.com>
2019-05-24 08:13:50 -0500

    Agregando rutas privadas

---
+++ src/components/PrivateRoute/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+import { getData } from "../../utils/storage";
+import { GITHUB_TOKEN } from "../../consts";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const githubToken = getData(GITHUB_TOKEN);
+
+  return(
+    <Route
+      {...rest}
+      render={props =>
+              githubToken
+              ? (<Component {...props}/>)
+              : <Redirect to={{ pathname: "/" }} />
+            }
+    />
+  )
+};
+
+export default PrivateRoute;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from "../pages/Login";
 import FourOFour from "../pages/FourOFour";
 import Home from '../pages/Home';
+import PrivateRoute from "../components/PrivateRoute";
 
 export default (
   <Router>
     <Switch>
       <Route exact path="/" component={Login} />
-      <Route path="/home" component={Home}/>
+      <PrivateRoute path="/home" component={Home}/>
       <Route component={FourOFour}/>
     </Switch>
   </Router>
